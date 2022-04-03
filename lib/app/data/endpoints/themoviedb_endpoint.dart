e7631441a407a858ecfda51919c388194785c7d8
Nicollas1705 <nicollasmota1705@gmail.com>
2022-04-03 15:46:45 -0300

    Added the url for images

--- lib/app/data/endpoints/themoviedb_endpoint.dart
+++ lib/app/data/endpoints/themoviedb_endpoint.dart
@@ -1,3 +1,4 @@
 abstract class TheMovieDbEndpoint {
-  static String get endpoint => "https://api.themoviedb.org/3/";
+  static String get url => "https://api.themoviedb.org/3/";
+  static String get urlImage => "https://image.tmdb.org/t/p/w300_and_h450_bestv2/";
 }
