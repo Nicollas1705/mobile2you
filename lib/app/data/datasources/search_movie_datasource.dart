125540cc2fc37db9f64e2447bb713bbdf4146f87
Nicollas1705 <nicollasmota1705@gmail.com>
2022-04-04 02:41:01 -0300

    Implemented search movie datasource

---
+++ lib/app/data/datasources/search_movie_datasource.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:mobile2you/app/data/datasources/search_movie_datasource_intreface.dart';
+import 'package:mobile2you/app/data/endpoints/themoviedb_endpoint.dart';
+import 'package:mobile2you/app/data/models/movie_model.dart';
+import 'package:mobile2you/app/domain/errors/datasource_errors.dart';
+import 'package:mobile2you/shared/constants/constants.dart';
+import 'package:mobile2you/shared/http_client/http_client_interface.dart';
+
+class SearchMovieDatasource implements ISearchMovieDatasource {
+  final IHttpClient client;
+  SearchMovieDatasource(this.client);
+
+  @override
+  Future<MovieModel> call(int movieId) async {
+    String url = TheMovieDbEndpoint.url;
+    url += "movie/$movieId?api_key=$API_KEY&language=en-US";
+
+    final result = await client.get(url);
+
+    if (result.statusCode == 404) {
+      throw NotFoundError();
+    } else if (result.statusCode != 200) {
+      throw InternalServerError();
+    } else if (result.body == null || result.body.toString().isEmpty) {
+      throw EmptyServerResultError();
+    } else if (json.decode(result.body)["status_code"] == 7) {
+      throw InvalidParamsRequestError();
+    }
+
+    final model = MovieModel.fromJson(result.body);
+    return model;
+  }
+}
