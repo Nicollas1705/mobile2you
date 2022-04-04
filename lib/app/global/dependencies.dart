aeacfbc38b351161fa29608b639726c3141ad8b0
Nicollas1705 <nicollasmota1705@gmail.com>
2022-04-04 06:35:22 -0300

    Added dependency injection with get_it

--- lib/app/global/dependencies.dart
+++ lib/app/global/dependencies.dart
@@ -1,10 +1,27 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile2you/app/data/datasources/search_movie_datasource.dart';
+import 'package:mobile2you/app/data/datasources/search_movie_datasource_intreface.dart';
+import 'package:mobile2you/app/data/sepositories/search_movie_repository.dart';
+import 'package:mobile2you/app/data/sepositories/search_movie_repository_interface.dart';
+import 'package:mobile2you/app/domain/usecases/search_movie_usecase.dart';
+import 'package:mobile2you/app/domain/usecases/search_movie_usecase_intreface.dart';
 import 'package:mobile2you/app/presenter/main_widget/main_widget.dart';
+import 'package:mobile2you/shared/dio/dio_adapter.dart';
+import 'package:mobile2you/shared/http_client/http_client_interface.dart';
 import 'package:mobile2you/shared/utils/close_app.dart';
+import 'package:get_it/get_it.dart';
 
 abstract class AppDependencies {
   static void init() {
     CloseApp.onBackPressed = DateTime.now();
+
+    GetIt locator = GetIt.I;
+    locator.registerLazySingleton<ISearchMovieUsecase>(() => SearchMovieUsecase(locator()));
+    locator.registerLazySingleton<ISearchMovieRepository>(() => SearchMovieRepository(locator()));
+    locator.registerLazySingleton<ISearchMovieDatasource>(() => SearchMovieDatasource(locator()));
+    locator.registerLazySingleton<IHttpClient>(() => DioAdapter(locator()));
+    locator.registerLazySingleton<Dio>(() => Dio());
 
     runApp(const MainWidget());
   }
