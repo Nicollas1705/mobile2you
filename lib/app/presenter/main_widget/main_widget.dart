28922c68939126c2db1c33cffd0de760f51eee7b
Nicollas1705 <nicollasmota1705@gmail.com>
2022-04-04 00:03:49 -0300

    Created the main widget

---
+++ lib/app/presenter/main_widget/main_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:mobile2you/app/presenter/pages/movie_page.dart';
+import 'package:mobile2you/app/presenter/themes/dark_theme.dart';
+import 'package:mobile2you/shared/constants/constants.dart';
+import 'package:mobile2you/shared/utils/app_scroll_behavior.dart';
+
+class MainWidget extends StatelessWidget {
+  const MainWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: DarkTheme.theme,
+      themeMode: ThemeMode.dark,
+      home: const MoviePage(movieId: MOVIE_ID),
+      debugShowCheckedModeBanner: false,
+      scrollBehavior: AppScrollBehavior(),
+    );
+  }
+}
