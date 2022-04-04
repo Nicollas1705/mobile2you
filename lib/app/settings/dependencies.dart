8f29f658a9ec75fb9fd72e83e822fbba92ec3fce
Nicollas1705 <nicollasmota1705@gmail.com>
2022-04-04 19:40:10 -0300

    Add dependency injection

--- lib/app/settings/dependencies.dart
+++ lib/app/settings/dependencies.dart
@@ -2,10 +2,16 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile2you/app/data/datasources/search_movie_datasource.dart';
 import 'package:mobile2you/app/data/datasources/search_movie_datasource_intreface.dart';
+import 'package:mobile2you/app/data/datasources/search_similar_movies_datasource.dart';
+import 'package:mobile2you/app/data/datasources/search_similar_movies_datasource_interface.dart';
 import 'package:mobile2you/app/data/sepositories/search_movie_repository.dart';
 import 'package:mobile2you/app/data/sepositories/search_movie_repository_interface.dart';
+import 'package:mobile2you/app/data/sepositories/search_similar_movies_repository.dart';
+import 'package:mobile2you/app/data/sepositories/search_similar_movies_repository_interface.dart';
 import 'package:mobile2you/app/domain/usecases/search_movie_usecase.dart';
 import 'package:mobile2you/app/domain/usecases/search_movie_usecase_intreface.dart';
+import 'package:mobile2you/app/domain/usecases/search_similar_movies_usecase.dart';
+import 'package:mobile2you/app/domain/usecases/search_similar_movies_usecase_interface.dart';
 import 'package:mobile2you/app/presenter/main_widget/main_widget.dart';
 import 'package:mobile2you/shared/dio/dio_adapter.dart';
 import 'package:mobile2you/shared/http_client/http_client_interface.dart';
@@ -20,6 +26,11 @@ abstract class AppDependencies {
     locator.registerLazySingleton<ISearchMovieUsecase>(() => SearchMovieUsecase(locator()));
     locator.registerLazySingleton<ISearchMovieRepository>(() => SearchMovieRepository(locator()));
     locator.registerLazySingleton<ISearchMovieDatasource>(() => SearchMovieDatasource(locator()));
+
+    locator.registerLazySingleton<ISearchSimilarMoviesUsecase>(() => SearchSimilarMoviesUsecase(locator()));
+    locator.registerLazySingleton<ISearchSimilarMoviesRepository>(() => SearchSimilarMoviesRepository(locator()));
+    locator.registerLazySingleton<ISearchSimilarMoviesDatasource>(() => SearchSimilarMoviesDatasource(locator()));
+
     locator.registerLazySingleton<IHttpClient>(() => DioAdapter(locator()));
     locator.registerLazySingleton<Dio>(() => Dio());
 
