30206149e5f559b125bafc2e1799eb94914ebd1c
Nicollas1705 <nicollasmota1705@gmail.com>
2022-04-04 03:18:54 -0300

    Created search movie repository

---
+++ lib/app/data/sepositories/search_movie_repository.dart
@@ -0,0 +1,24 @@
+import 'package:mobile2you/app/data/datasources/search_movie_datasource_intreface.dart';
+import 'package:mobile2you/app/data/models/movie_model.dart';
+import 'package:mobile2you/app/data/sepositories/search_movie_repository_interface.dart';
+import 'package:mobile2you/app/domain/errors/datasource_errors.dart';
+import 'package:mobile2you/app/domain/errors/repository_errors.dart';
+
+class SearchMovieRepository implements ISearchMovieRepository {
+  final ISearchMovieDatasource datasource;
+  SearchMovieRepository(this.datasource);
+
+  @override
+  Future<MovieModel> call(int movieId) async {
+    late MovieModel movieModel;
+    try {
+      movieModel = await datasource(movieId);
+    } on DatasourceError {
+      rethrow;
+    } catch (e) {
+      throw UnknowRepositoryError();
+    }
+
+    return movieModel;
+  }
+}
