1057b8da0b4228ddf9c6cc45d6d6330597219d54
Nicollas1705 <nicollasmota1705@gmail.com>
2022-04-04 20:18:29 -0300

    Add language and genres constants

--- lib/shared/constants/constants.dart
+++ lib/shared/constants/constants.dart
@@ -2,3 +2,25 @@
 
 const int MOVIE_ID = 286217;
 const String API_KEY = "a9d5ad9e22eebf95ce934f0484e31fbd";
+const String API_LANG = "en-US";
+const List<Map<String, dynamic>> GENRES = [
+  {"id": 28, "name": "Action"},
+  {"id": 12, "name": "Adventure"},
+  {"id": 16, "name": "Animation"},
+  {"id": 35, "name": "Comedy"},
+  {"id": 80, "name": "Crime"},
+  {"id": 99, "name": "Documentary"},
+  {"id": 18, "name": "Drama"},
+  {"id": 10751, "name": "Family"},
+  {"id": 14, "name": "Fantasy"},
+  {"id": 36, "name": "History"},
+  {"id": 27, "name": "Horror"},
+  {"id": 10402, "name": "Music"},
+  {"id": 9648, "name": "Mystery"},
+  {"id": 10749, "name": "Romance"},
+  {"id": 878, "name": "Science Fiction"},
+  {"id": 10770, "name": "TV Movie"},
+  {"id": 53, "name": "Thriller"},
+  {"id": 10752, "name": "War"},
+  {"id": 37, "name": "Western"}
+];
